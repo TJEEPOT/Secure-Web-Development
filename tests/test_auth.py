538be1b7c19abb9af557ec65157de895f2f8f1a3
TJEEPOT <TJEEPOT@gmail.com>
2021-04-03 14:39:56 +0100

    move test for authenticate users from test_auth to test_db

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -31,10 +31,6 @@ class MyTestCase(unittest.TestCase):
     def test_generate_salt(self):
         self.assertIsNotNone(auth.generate_salt())
 
-    def test_authenticate_user(self):
-        with app.app_context():
-            self.assertEqual(0, auth.authenticate_user("aking", "password"))
-
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,21 @@
 import unittest
 
+import db
+from blog import app
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_get_login_correct(self):
+        with app.app_context():
+            self.assertEqual(0, db.get_login("aking", "password"))
+
+    def test_get_login_incorrect_user(self):
+        with app.app_context():
+            self.assertEqual(None, db.get_login("IDoNotExist", "password"))
+
+    def test_get_login_incorrect_password(self):
+        with app.app_context():
+            self.assertEqual(None, db.get_login("aking", "ThisIsNotThePassword"))
 
 
 if __name__ == '__main__':
