a60d8028147414e2d061ddfae45481957c65953b
TJEEPOT <TJEEPOT@gmail.com>
2021-04-04 12:34:27 +0100

    moved test_db.py back to using unittest as pytest was breaking grouped tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,54 +1,76 @@
-import pytest
+import time
+import unittest
 import db
 from blog import app
 
 
-# CS: Make sure the tests run in the app environment
-@pytest.fixture
-def app_context():
-    with app.app_context():
-        yield
-
-
 # TODO: CS: Probably going to need to be rewritten if database is changed.
-def test_get_db(app_context):
-    assert db.get_db() is not None
+class MyTestCase(unittest.TestCase):
+    def test_get_db(self):
+        with app.app_context():
+            self.assertIsNotNone(db.get_db())
 
+    def test_query_db(self):
+        with app.app_context():
+            # check something is retrieved
+            self.assertIsNotNone(db.query_db("SELECT username FROM users WHERE userid = 0"))
 
-def test_query_db(app_context):
-    # check something is retrieved
-    assert db.query_db("SELECT username FROM users WHERE userid = 0", one=False) is not None
-    # check the correct thing is retrieved
-    assert db.query_db("SELECT username FROM users WHERE userid = 0", one=False)[0]["username"] == "aking"
-    # check it works for one=True also
-    assert db.query_db("SELECT username FROM users WHERE userid = 0", one=True)["username"] == "aking"
+            # check args works correctly
+            query = "SELECT username FROM users WHERE userid=?"
+            self.assertIsNotNone(db.query_db(query, (0,)))
 
+            # check the correct thing is retrieved
+            query = "SELECT username FROM users WHERE userid = 0"
+            self.assertEqual('aking', db.query_db(query)[0]['username'])
 
-def test_get_login(app_context):
-    # these should all take the same amount of time to execute (one second)
-    # check the account is returned
-    assert db.get_login("aking", "password") == 0
-    # check the function returns None on incorrect username
-    assert db.get_login("IDoNotExist", "password") is None
-    # check the function returns None on incorrect password
-    assert db.get_login("aking", "ThisIsNotThePassword") is None
+            # check it works for one=True also
+            query = "SELECT username FROM users WHERE userid = 0"
+            self.assertEqual('aking', db.query_db(query, one=True)['username'])
 
+    def test_get_login(self):
+        with app.app_context():
+            # check the account is returned in around one second
+            start_time = time.time()
+            account = db.get_login("aking", "password")
+            time_diff = time.time() - start_time
 
-def test_get_all_posts(app_context):
-    # check something is retrieved
-    assert db.get_all_posts() is not None
+            self.assertEqual(0, account)
+            self.assertLess(0.95, time_diff)
+            self.assertGreater(1.15, time_diff)
 
+            # check the function returns None on incorrect username in around one second
+            start_time = time.time()
+            account = db.get_login("IDoNotExist", "password")
+            time_diff = time.time() - start_time
 
-# get_posts can not be tested.
+            self.assertIsNone(account)
+            self.assertLess(0.95, time_diff)
+            self.assertGreater(1.15, time_diff)
 
-# TODO: CS: The rest of these are going to need to be written after db.py is complete.
+            # check the function returns None on incorrect password in around one second
+            start_time = time.time()
+            account = db.get_login("aking", "ThisIsNotThePassword")
+            time_diff = time.time() - start_time
 
-# def test_add_post(app_context):
+            self.assertIsNone(account)
+            self.assertLess(0.95, time_diff)
+            self.assertGreater(1.15, time_diff)
 
-# def test_get_email(app_context):
+    def test_get_all_posts(self):
+        with app.app_context():
+            # check something is retrieved
+            self.assertIsNotNone(db.get_all_posts())
 
-# def test_get_users(app_context):
+    # get_posts can not be tested.
+
+    # TODO: CS: The rest of these are going to need to be written after db.py is complete.
+
+    # def test_add_post(app_context):
+
+    # def test_get_email(app_context):
+
+    # def test_get_users(app_context):
 
 
 if __name__ == '__main__':
-    pytest.main()
+    unittest.main()
