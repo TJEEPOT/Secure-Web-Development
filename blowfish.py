318455be7f6f87d4afbcb394ce36495041cf3d3e
Steven Diep <steven_diep@hotmail.co.uk>
2021-04-27 23:21:31 +0100

    removed creation of new p and s boxes. now only generates subkeys for p box only by round robin XORing the p val and actually utilises the key..

--- blowfish.py
+++ blowfish.py
@@ -12,6 +12,8 @@ History : 09/04/2021 - v1.0 - Create project file.
           12/04/2021 - v1.2 - Added mode of operation for Blowfish cipher
           18/04/2021 - v1.3 - Used Counter mode
           18/04/2021 - v1.4 - Created unit tests
+          27/04/2021 - v1.5 - Removed generation of P and S boxes using encryption method, only sub-keys from P-boxes
+                              are generated.
 
 """
 
@@ -34,24 +36,13 @@ class BlowyFishy:
             raise Exception("Key length must be between 32 - 448 bits long.")
 
         new_p_box = [None] * 18
+        element = 0
         key_length = len(key)
         for i in range(len(constants.p_box)):
-            new_p_box[i] = constants.p_box[i] ^ ord(key[i % key_length])
-
-        lhs, rhs = 0, 0
-        # Changes all p-boxes
-        for i in range(0, len(constants.p_box), 2):
-            left_p, right_p = self.encrypt(lhs, rhs)
-            new_p_box[i] = left_p
-            new_p_box[i + 1] = right_p
-
-        # Changes all s-boxes
-        new_s_box = [[None] * 256] * 4
-        for i in range(len(constants.s_box)):
-            for j in range(0, len(constants.s_box[i]), 2):
-                left_s, right_s = self.encrypt(lhs, rhs)
-                new_s_box[i][j] = left_s
-                new_s_box[i][j + 1] = right_s
+            input_key = (ord(key[element % key_length]) << 24) + (ord(key[(element + 1) % key_length]) << 16) + \
+                        (ord(key[(element + 2) % key_length]) << 8) + ord(key[(element + 3) % key_length])
+            new_p_box[i] = constants.p_box[i] ^ input_key
+            element += 4
 
     def encrypt(self, lhs, rhs):
         """Encrypts a block size of 64 bit plain text using Blowfish
