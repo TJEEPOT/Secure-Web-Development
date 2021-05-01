62d32b00274a39d6c9428018955a86a008b372fc
Sam <ctg18jvu@uea.ac.uk>
2021-05-01 14:20:41 +0100

    Added create dir if not exist

---
+++ blogging.py
@@ -0,0 +1,72 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Logger for blog events
+File    : blogging.py
+Date    : Saturday 03 April 2021
+Desc.   : Logging wrapper functions for logging various blog events
+History : 03/04/2021 - v1.0 - Basic functions.
+"""
+
+__author__ = "Martin Siddons, Chris Sutton, Sam Humphreys, Steven Diep"
+__copyright__ = "Copyright 2021, CMP-UG4"
+__credits__ = ["Martin Siddons", "Chris Sutton", "Sam Humphreys", "Steven Diep"]
+__version__ = "1.0"
+__email__ = "gny17hvu@uea.ac.uk"
+__status__ = "Development"  # or "Production"
+
+import logging
+import datetime
+import os
+
+previous_date = None
+
+
+def create_dir_if_not_exist():
+    if not os.path.isdir("logs/"):
+        os.mkdir('logs/')
+        
+
+def get_file_location():
+    create_dir_if_not_exist()
+    base = "logs/"
+    date = get_date()
+    full_location = base + date + ".log"
+    return full_location
+
+
+def get_date():
+    date = datetime.date.today()
+    return date.strftime('%Y-%m-%d')
+
+
+# TODO put logging into the request wrapper function as well as specific functions to be called
+
+def config_logger():
+    logging.basicConfig(filename=get_file_location(),
+                        format='%(asctime)s : %(levelname)s : %(message)s',
+                        level=logging.INFO)
+    global previous_date
+    previous_date = get_date()
+
+
+def log_user_activity_happy(user_id: str, ip: str, activity: str):
+    if previous_date is not get_date():
+        config_logger()
+    logging.info(f'[User: {user_id},{ip}, {activity}]')
+
+
+def log_user_activity_unhappy(user_id: str, ip: str, activity: str):
+    if previous_date is not get_date():
+        config_logger()
+    logging.warning(f'[User: {user_id}, {ip}, {activity}]')
+
+
+def log_this(log_me: str):
+    logging.info(log_me)
+
+
+if __name__ == '__main__':
+    log_user_activity_unhappy("0", "100.000.000.000", "oops")
+    log_user_activity_happy("0", "100.001.000.000", "yay")
+
