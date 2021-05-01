ca9d08ac914cd161b50e08896c6c05fd65a64b29
TJEEPOT <TJEEPOT@gmail.com>
2021-05-01 14:19:07 +0100

    fix comment

--- blowfish.py
+++ blowfish.py
@@ -15,6 +15,7 @@ History : 09/04/2021 - v1.0 - Create project file.
           27/04/2021 - v1.5 - Removed generation of P and S boxes using encryption method, only sub-keys from P-boxes
                               are generated.
           27/04/2021 - v1.6 - Added helper / wrapper functions for encrypt and decrypt
+"""
 
 __author__ = "Martin Siddons, Chris Sutton, Sam Humphreys, Steven Diep"
 __copyright__ = "Copyright 2021, CMP-UG4"
